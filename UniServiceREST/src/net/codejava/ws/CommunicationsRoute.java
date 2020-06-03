452bdb5e187044cc85274263113c0ab1e79dc6a2
andiub97 <43032657+andiub97@users.noreply.github.com>
2020-06-04 00:27:48 +0200

    updServer

---
+++ UniServiceREST/src/net/codejava/ws/CommunicationsRoute.java
@@ -0,0 +1,49 @@
+package net.codejava.ws;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/getCommunications")
+public class CommunicationsRoute {
+
+  
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public String Read() throws ClassNotFoundException, SQLException {
+  	Class.forName("org.sqlite.JDBC");
+		Connection con=DriverManager.getConnection("jdbc:sqlite:/home/andreadiubaldo/eclipse-workspaceWeb/UniServiceREST/uniService.db");
+		
+		Statement st=con.createStatement();
+		
+		//reading records
+		System.out.println("Reading records:");
+
+		ResultSet rs=st.executeQuery("select * from communications");
+		
+		return stringa(rs);
+  }
+  
+  private String stringa (ResultSet rs) throws SQLException {
+  	ArrayList<String> a = new ArrayList<>(); 
+		
+		while(rs.next()){
+			a.add("{\"year \": \"" +  rs.getString("year") + "\""   
+					 + ",\"day \": \""  + rs.getString("day") + "\""
+					 + ",\"communication \": \"" +  rs.getString("communication") + "\""+ "}");
+		}
+		return a.toString();
+  	
+  }
+
+}
+
+
